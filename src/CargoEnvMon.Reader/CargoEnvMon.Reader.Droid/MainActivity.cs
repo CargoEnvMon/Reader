bff563a574cfea57953162d59c388628f3838770
bymse <egorstrelov@gmail.com>
2022-03-27 20:28:02 +0300

    Init

---
+++ src/CargoEnvMon.Reader/CargoEnvMon.Reader.Droid/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace CargoEnvMon.Reader.Droid
+{
+    [Activity(Label = "CargoEnvMon.Reader", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
