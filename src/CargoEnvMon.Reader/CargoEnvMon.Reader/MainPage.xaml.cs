e801dd3427769f5b8886037eb47a73624316e7a8
bymse <egorstrelov@gmail.com>
2022-03-31 00:55:36 +0300

    Basic ui impl

--- src/CargoEnvMon.Reader/CargoEnvMon.Reader/MainPage.xaml.cs
+++ src/CargoEnvMon.Reader/CargoEnvMon.Reader/MainPage.xaml.cs
@@ -1,17 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using CargoEnvMon.Reader.ViewModels;
 
 namespace CargoEnvMon.Reader
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage
     {
+        private string buttonText;
+        private bool isStarted;
+
+        private bool IsStarted
+        {
+            get => isStarted;
+            set
+            {
+                isStarted = value;
+                OnPropertyChanged(nameof(EnableEntry));
+            }
+        }
+
+        public bool EnableEntry => !IsStarted;
+
+        public string ButtonText
+        {
+            get => buttonText;
+            set
+            {
+                buttonText = value;
+                OnPropertyChanged(nameof(ButtonText));
+            }
+        }
+
+        public string ShipmentId { get; set; }
+
+        public ObservableCollection<CargoRequestViewModel> CargoRequestResults { get; } = new();
+
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+            ButtonText = "Start";
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            IsStarted = !IsStarted;
+            if (IsStarted)
+            {
+                ButtonText = "Stop";
+            }
+            else
+            {
+                ButtonText  = "Start";
+            }
+            
+            CargoRequestResults.Add(new CargoRequestViewModel(CargoRequestResults.Count, new Random().Next().ToString(), isStarted));
         }
     }
 }
